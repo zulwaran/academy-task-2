fe5064687420548af7709135a79e3a3072ff3dbe
zulwaran <81923601+zulwaran@users.noreply.github.com>
2021-04-14 21:38:22 +0700

    Третий коммит
    Исправил баг с выводом неправильных элементов, изменил структуру создаваемой формы, добавил стили

--- js/main.js
+++ js/main.js
@@ -13,107 +13,121 @@ button.addEventListener('click', function () {
         showFields(pars);
 
         function populateHeader(jsonObj) {
-            var node = document.getElementById('header');
+            var node = document.getElementById('form');
             while (node.hasChildNodes()) {
                 node.removeChild(node.firstChild);
             }
-            var myH1 = document.createElement('h1');
-            myH1.textContent = jsonObj['name'];
-            header.appendChild(myH1);
+            var section = document.getElementById('form');
+            var title = document.createElement('div');
+            title.textContent = jsonObj['name'];
+            title.className = "form__title";
+            section.appendChild(title);
         }
 
         function showFields(jsonObj) {
-            var node = document.getElementById('section');
-            while (node.hasChildNodes()) {
-                node.removeChild(node.firstChild);
-            }
             var fields = jsonObj['fields'];
             var references = jsonObj['references'];
             var buttons = jsonObj['buttons'];
+            var section = document.getElementById('form');
+            var formInner = document.createElement('div');
+            formInner.className = 'form__inner';
+            section.appendChild(formInner);
+            var formFields = document.createElement('div');
+            formFields.className = 'form__fields';
+            formInner.appendChild(formFields);
+            var formReferences = document.createElement('div');
+            formReferences.className = 'form__references';
+            formInner.appendChild(formReferences);
+            var formButtons = document.createElement('div');
+            formButtons.className = 'form__buttons';
+            formInner.appendChild(formButtons);
+
 
             for (var i = 0; i < fields.length; i++) {
-                var myDiv = document.createElement('div');
-                var myLabel = document.createElement('label');
-                var myInp = document.createElement('input');
-                var mySelect = document.createElement('select');
-                var myTextarea = document.createElement('textarea');
-                myLabel.textContent = fields[i].label;
-                myDiv.appendChild(myLabel);
+                var fieldsDiv = document.createElement('div');
+                var fieldsLabel = document.createElement('label');
+                var fieldsInp = document.createElement('input');
+                var fieldsSelect = document.createElement('select');
+                var fieldsTextarea = document.createElement('textarea');
+                fieldsLabel.textContent = fields[i].label;
+                fieldsDiv.className = 'field';
+                formFields.appendChild(fieldsDiv);
+                fieldsDiv.appendChild(fieldsLabel);
                 switch (fields[i].input.type) {
                     case 'number':
-                        myInp.type = fields[i].input.type;
-                        myInp.required = fields[i].input.required;
+                        fieldsInp.type = fields[i].input.type;
+                        fieldsInp.required = fields[i].input.required;
                         if (typeof (fields[i].input.mask) !== "undefined") {
-                            myInp.placeholder = fields[i].input.mask;
+                            fieldsInp.placeholder = fields[i].input.mask;
                         }
-                        myDiv.appendChild(myInp);
+                        fieldsInp.className = fields[i].input.type;
+                        fieldsDiv.appendChild(fieldsInp);
                         break;
                     case 'email':
-                        myInp.type = fields[i].input.type;
-                        myInp.required = fields[i].input.required;
-                        myInp.placeholder = fields[i].input.placeholder;
-                        myDiv.appendChild(myInp);
+                        fieldsInp.type = fields[i].input.type;
+                        fieldsInp.required = fields[i].input.required;
+                        fieldsInp.placeholder = fields[i].input.placeholder;
+                        fieldsDiv.appendChild(fieldsInp);
                         break;
                     case 'technology':
-                        mySelect.type = fields[i].input.type;
-                        mySelect.required = fields[i].input.required;
-                        mySelect.multiple = true;
-                        myDiv.appendChild(mySelect);
+                        fieldsSelect.type = fields[i].input.type;
+                        fieldsSelect.required = fields[i].input.required;
+                        fieldsSelect.multiple = true;
+                        fieldsDiv.appendChild(fieldsSelect);
                         if (typeof (fields[i].input.technologies) !== "undefined") {
                             for (let elem in fields[i].input.technologies) {
-                                var myOptions = document.createElement('option');
+                                var fieldsOptions = document.createElement('option');
                                 y = (fields[i].input.technologies[elem]);
-                                myOptions.innerHTML = y;
-                                mySelect.appendChild(myOptions);
+                                fieldsOptions.innerHTML = y;
+                                fieldsSelect.appendChild(fieldsOptions);
                             }
                         }
                         break;
                     case 'file':
-                        myInp.type = fields[i].input.type;
-                        myInp.required = fields[i].input.required;
-                        myInp.multiple = true;
+                        fieldsInp.type = fields[i].input.type;
+                        fieldsInp.required = fields[i].input.required;
+                        fieldsInp.multiple = true;
                         if (typeof (fields[i].input.filetype) !== "undefined") {
                             z = [];
                             for (let elem in fields[i].input.filetype) {
                                 z.push("." + fields[i].input.filetype[elem]);
                             }
-                            myInp.accept = z;
+                            fieldsInp.accept = z;
                         }
-                        myDiv.appendChild(myInp);
+                        fieldsDiv.appendChild(fieldsInp);
                         break;
                     case 'textarea':
-                        myInp.type = 'textarea';
-                        myInp.required = fields[i].input.required;
-                        myInp.cols = '20';
-                        myInp.rows = '3';
-                        myDiv.appendChild(myTextarea);
+                        fieldsInp.type = 'textarea';
+                        fieldsInp.required = fields[i].input.required;
+                        fieldsInp.cols = '20';
+                        fieldsInp.rows = '3';
+                        fieldsDiv.appendChild(fieldsTextarea);
                         break;
                     case 'color':
                         for (elem of fields[i].input.colors) {
-                            myInp = document.createElement('input');
-                            myInp.type = fields[i].input.type;
-                            myInp.value = elem;
-                            myDiv.appendChild(myInp);
+                            fieldsInp = document.createElement('input');
+                            fieldsInp.type = fields[i].input.type;
+                            fieldsInp.value = elem;
+                            fieldsDiv.appendChild(fieldsInp);
                         }
 
                         break;
                     default:
-                        myInp.type = fields[i].input.type;
-                        myInp.required = fields[i].input.required;
+                        fieldsInp.type = fields[i].input.type;
+                        fieldsInp.required = fields[i].input.required;
                         if (typeof (fields[i].input.placeholder) !== "undefined") {
-                            myInp.placeholder = fields[i].input.placeholder;
+                            fieldsInp.placeholder = fields[i].input.placeholder;
                         }
-                        myDiv.appendChild(myInp);
+                        fieldsDiv.appendChild(fieldsInp);
                         break;
                 }
-                section.appendChild(myDiv);
             }
 
             for (var i = 0; i < references.length; i++) {
-                var refDiv = document.createElement('div');
                 var refInp = document.createElement('input');
+                var refDiv = document.createElement('div');
                 var refA = document.createElement('a');
-                var refP = document.createElement('p');
+                formReferences.appendChild(refDiv);
                 if (Object.keys(references[i]) == 'input') {
                     var input = references[i].input;
                     refInp.type = input.type;
@@ -121,29 +135,25 @@ button.addEventListener('click', function () {
                         refInp.checked = true;
                     } else {
                         refInp.checked = false;
-                    }
+                    };
                     refInp.required = input.required;
                     refDiv.appendChild(refInp);
-                    section.appendChild(refDiv);
                 } else {
-                    refP.innerHTML = references[i]['text without ref'];
+
                     refA.innerHTML = references[i]['text'];
                     refA.href = references[i]['ref'];
-                    if (refP.innerHTML !== 'undefined') {
-                        refDiv.appendChild(refP);
+                    if (references[i]['text without ref'] !== undefined) {
+                        refDiv.innerHTML = references[i]['text without ref'] + ' ';
                     }
-                    if (refA.innerHTML !== 'undefined') {
+                    if (refA.innerHTML !== undefined) {
                         refDiv.appendChild(refA);
                     }
-                    section.appendChild(refDiv);
                 }
             }
             for (var i = 0; i < buttons.length; i++) {
-                var btnDiv = document.createElement('div');
                 var myButton = document.createElement('button');
                 myButton.textContent = buttons[i].text;
-                btnDiv.appendChild(myButton);
-                section.appendChild(btnDiv);
+                formButtons.appendChild(myButton);
             }
         }
     };
